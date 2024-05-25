e68ad019a103e029d3cdbb2bf89e08bf370a93e2
mohpfd96 <mohpfd96@gmail.com>
2024-05-25 23:25:30 +0330

    message

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -5,7 +5,7 @@ const contactController = require("../controllers/contact.controller");
 //api/contacts
 router.post("/:userId", contactController.createContact);
 router.get("/:userId", contactController.getContacts);
-router.get("/", contactController.getAllContacts); //admin
+router.get("/", contactController.getAllContacts); // Admin Access
 router.get("/:id", contactController.getContact);
 router.put("/:id", contactController.updateContact);
 router.delete("/:id", contactController.deleteContact);
